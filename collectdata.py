2a0753ff805f5ae572b792a9dda8a854fad6eaeb
yewon-yun <yunyw@bu.edu>
2026-01-02 19:31:34 +0900

    added some helper functions to help stabilize the data sets
    we take the distance to the wrist and divided by the original wrist to midle finger ratio

--- collectdata.py
+++ collectdata.py
@@ -7,3 +7,23 @@ import math
 
 mp_hands = mp.solution.hands
 
+#standardizing hand datasets bc they may vary in size and distance-------------------------------------------------------
+
+def _dist(a, b):
+    return math.hypot(a.x - b.x, a.y - b.y) #the standard wrist to middlefinger distance for each hand
+
+def extract_features(landmarks):
+    feature = []
+    scale = _dist(landmarks[0], landmarks[9])
+
+    for i in landmarks:
+        feature.append((i.x-landmarks[0].x)/scale)
+        feature.append((i.x-landmarks[0])/scale)
+
+    return feature
+
+
+#snapshotting process---------------------------------------------------------------------------------------------------
+
+def main():
+    
